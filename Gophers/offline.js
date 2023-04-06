56f2da7448491152c3ae2860c30372b23ba4f287
hamster1230 <130032399+hamster1230@users.noreply.github.com>
2023-04-06 14:19:29 +0800

    Add files via upload

---
+++ Gophers/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1680761356,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/打地鼠_背景-sheet0.png",
+		"images/gophers-sheet0.png",
+		"images/gophers-sheet1.png",
+		"images/hole-sheet0.png",
+		"images/star-sheet0.png",
+		"images/hammer-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
